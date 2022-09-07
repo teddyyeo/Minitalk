84ee708e0b09fb95a20526790c85308f033c9bbc
tayeo <tayeo@student.42seoul.kr>
2022-09-07 21:59:32 +0900

    trhid commit bonus almost done

--- client.c
+++ client.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayeo <tayeo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 19:32:24 by tayeo             #+#    #+#             */
+/*   Updated: 2022/09/07 21:10:01 by tayeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-void send_string(char *str, int pid)
+void	send_string(char *str, int pid)
 {
 	while (*str)
 	{
@@ -8,18 +20,17 @@ void send_string(char *str, int pid)
 		{
 			if((*str & 128) == 128)
 			{
-				write(1, "1", 2);
 				kill(pid, SIGUSR1);
 				usleep(100);
 			}
 			else
 			{
-				write(1, "0", 2);
 				kill(pid, SIGUSR2);
 				usleep(100);
 			}
 			*str = *str << 1;
 		}
+		str++;
 	}
 }
 
@@ -27,34 +38,35 @@ void send_size(size_t len, int pid)
 {
 	for (int i = 0; i < 32; i++)
 	{
-		if((len & 2148473648UL) == 2147483648UL)
+		if ((len & 2148473648UL) == 2147483648UL)
 		{
-			write(1, "1", 2);
+			write(1, "1", 1);
 			kill(pid, SIGUSR1);
 			usleep(100);
 		}
 		else
 		{
-			write(1, "0", 2);
+			write(1, "0", 1);
 			kill(pid, SIGUSR2);
 			usleep(100);
 		}
 		len = len << 1;
 	}
-
 }
 
 int	main(int argc, char **argv)
 {
+	int		pid;
+	size_t	len;
+
 	if (argc != 3)
 	{
 		ft_printf("Argument Error\n");
 		return (0);
 	}
-	//ft_printf("argv[0]: %s\n", argv[0]);
-	int pid = ft_atoi(argv[1]);
-	size_t len = ft_strlen(argv[2]);
-
+	pid = ft_atoi(argv[1]);
+	len = ft_strlen(argv[2]);
 	send_size(len, pid);
+	send_string(argv[2], pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayeo <tayeo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 19:33:58 by tayeo             #+#    #+#             */
+/*   Updated: 2022/09/07 21:59:03 by tayeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
+#include <stdio.h> //TODO remove!!!!!!!!!!
 
 void showbit(char c)
 {
-	write(1, "State:\n", ft_strlen("State:\n"));
-	char b = c;
+	char	b = c;
+
 	for (int i = 0; i < 8; i++)
 	{
-		if((b & 128) == 128)
+		if ((b & 128) == 128)
 		{
 			write(1, "1", 2);
 		}
@@ -19,34 +32,78 @@ void showbit(char c)
 	write(1, "\n", 2);
 }
 
-void	handler(int sig)
+size_t	size_handler(int sig)
 {
-	static char c = 'A';
-	if (c == 'A')
-		showbit(c);
+	static size_t	len;
+
 	if (sig == SIGUSR1)
 	{
-		c = c << 1;
-		c = c | 1;
-		showbit(c);
-		//write(1, "1", ft_strlen("1"));
+		len = len << 1;
+		len = len | 1;
 	}
 	if (sig == SIGUSR2)
+		len = len << 1;
+	return (len);
+}
+
+char	*str_handler(int sig, char *str, size_t len)
+{
+	static char*	start;
+	static int	bit_idx;
+	static size_t	l;
+
+	if (start == NULL)
+		start = str;
+	if (bit_idx == 8)
 	{
-		c = c << 1;
-		showbit(c);
-		//write(1, "0", ft_strlen("1"));
+		l++;
+		if (l == len)
+			printf("%s\n", start);
+		bit_idx = 0;
+		str++;
 	}
-	if (sig == SIGINT)
-		write(1, &c, 1);
+	if (sig == SIGUSR1)
+	{
+		*str = *str << 1;
+		*str = *str | 1;
+	}
+	if (sig == SIGUSR2)
+		*str = *str << 1;
+	bit_idx++;
+	return (str);
+}
+
+void	handler(int sig)
+{
+	static char	size_flag;
+	static char	bit_idx;
+	static char	*str;
+	size_t		len;
+
+	len = 0;
+	if (bit_idx < 32 && size_flag == 0)
+	{
+		len = size_handler(sig);
+		bit_idx++;
+		if (bit_idx == 32)
+		{
+			printf("len: %zu\n", len);
+			size_flag = 1;
+		}
+		return ;
+	}
+	if (str == NULL)
+		str = malloc(len + 1);
+	str = str_handler(sig, str, len + 1);
 }
 
 int	main(void)
 {
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
-	signal(SIGINT, handler);
 	ft_printf("PID: %d\n", getpid());
-	while(1){}
+	while (1)
+	{
+	}
 	return (0);
 }
